b0bcaddb2692fbc4367934f4f6898d553a2b37c8
Mike Botsko <botsko@gmail.com>
2018-12-22 10:35:03 -0800

    Balance materials

--- src/main/java/com/helion3/darkmythos/Materials.java
+++ src/main/java/com/helion3/darkmythos/Materials.java
@@ -30,6 +30,6 @@ public class Materials {
     // name, harvest level, durability, mining speed, damage, enchantability
     public static final Item.ToolMaterial DARK_CRYSTAL = EnumHelper.addToolMaterial("DARK_CRYSTAL", 3, 256, 13, 4, 20);
     public static final Item.ToolMaterial SUPERIOR_DARK_CRYSTAL = EnumHelper.addToolMaterial("SUPERIOR_DARK_CRYSTAL", 3, 2048, 9, 10, 30);
-    public static final Item.ToolMaterial LEGENDARY_DARK_CRYSTAL = EnumHelper.addToolMaterial("LEGENDARY_DARK_CRYSTAL", 3, 16384, 40, 40, 40);
-    public static final Item.ToolMaterial MYTHICAL_DARK_CRYSTAL = EnumHelper.addToolMaterial("MYTHICAL_DARK_CRYSTAL", 3, 65000, 80, 100, 100);
+    public static final Item.ToolMaterial LEGENDARY_DARK_CRYSTAL = EnumHelper.addToolMaterial("LEGENDARY_DARK_CRYSTAL", 3, 16384, 40, 20, 40);
+    public static final Item.ToolMaterial MYTHICAL_DARK_CRYSTAL = EnumHelper.addToolMaterial("MYTHICAL_DARK_CRYSTAL", 3, 65000, 80, 40, 100);
 }
